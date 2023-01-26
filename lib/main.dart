a120bf0b9c7e4f3501336a2eb9c3227f0091ceb1
ariqluth <ttzluthfi@gmail.com>
2023-01-26 21:07:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'Home Screen/home_screen.dart';
+import 'Splash Screen/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Resturant App',
+      routes: {
+        "/": (context) => SplashScreen(),
+        "/home": (context) => HomePage()
+      },
+      theme: ThemeData(
+        fontFamily: 'nunito',
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
